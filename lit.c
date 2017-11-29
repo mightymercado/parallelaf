7add79b83d7f132e4313d2faa571150f0f49859b
Ken Mercado <mightymercado@gmail.com>
2017-11-29 17:55:40 +0800

    Update lit.c

--- lit.c
+++ lit.c
@@ -7,14 +7,33 @@
 #include <signal.h>
 #include <stdint.h>
 #include <unistd.h>
-
+typedef unsigned __int128 uint128_t;
 #define N 5
 #define STATES (1 << (N * N))
 
-// unpacked toggle masks
+/*
+    These are called "toggle masks".
+*/
 int mask[] = {
     35, 71, 142, 284, 536, 1121, 2274, 4548, 9096, 17168, 35872, 72768, 145536, 291072, 549376,
     1147904, 2328576, 4657152, 9314304, 17580032, 3178496, 7405568, 14811136, 29622272, 25690112
+};
+
+uint128_t cmask[5] = {
+    443677736902923512 * (uint128_t) (1e14)
+    +139394679320,
+
+    142103640876514133 * (uint128_t) (1e16)
+    +6035471552168720,
+
+    454731650804845227 * (uint128_t) (1e17)
+    +53135089669399040,
+
+    145514128257550472 * (uint128_t) (1e19)
+    +8100322869420769280,
+
+    402922264199749179 * (uint128_t) (1e19)
+    +4093243870648729600
 };
 
 // prv[state] is the toggle used to arrive at state
@@ -29,18 +48,32 @@ unsigned char set[1<<22];
 #define insert(i) set[i >> 3] |= 1 << (i & 7)
 #define check(i) ((set[i >> 3] & (1 << (i & 7))) != 0)
 
-#define MAX_CORES 8
+#define MAX_CORES 4
 int cores;
-int arr1[1<<25];
-int arr2[MAX_CORES][1<<25];
+int arr1[MAX_CORES][1<<22];
+int arr2[MAX_CORES][1<<22];
 
 // next_level in bfs tree
 int *next[MAX_CORES];
 int next_size[MAX_CORES];
 
 // current_level in bfs tree
-int *curr;
-int curr_size;
+int *curr[MAX_CORES];
+int curr_size[MAX_CORES];
+
+int size = 0;
+int start = 0;
+#define lim 2097152
+
+// Queue via Circular array
+int q[lim];
+int pop() {
+    size--;
+    return q[start++ & (lim-1)];
+}
+void push(int x) {
+    q[(start + size++) & (lim-1)] = x;
+}
 
 void render(uint32_t board) {
     uint32_t i, j;
@@ -53,28 +86,16 @@ void render(uint32_t board) {
 }
 
 void *partial_consumer(void *data) {
-    // all partial consumers process mutually exclusive portions of the current_level queues
-    // but they also have different next_level queues which will then be merged when all partial consumers are done
-
-    // unpack
-    // first 30 bits = start
-    // second 30 bits = end
-    // remaining 4 bits = cores
-    int start = (uint64_t) data >> 34;
-    int end = ((uint64_t) data >> 4) & ((1 << 30) - 1);
-    int core = (uint64_t) data & 0xf;
-    
-    for (int j = start; j < end; j++) {
-        // get state
-        int u = curr[j];
+    int k = (int) data;
+    for (int j = 0; j < curr_size[k]; j++) {
+        int u = curr[k][j];
         for (int i = 24; i >= 0; --i) {
-            // get next_state
             int x = u ^ mask[i];
-            if (!test(x)) {
-                // add to it's own next_level or queue
-                next[core][next_size[core]++] = x;
+            if (!test(x)) {  
                 visit(x);
+                next[k][next_size[k]++] = x;
                 prv[x] = i;
+            } else {
             }
         }
     }
@@ -99,94 +120,71 @@ int max(int a, int b) {
     if (a > b) return a;
     else return b;
 }
-void bfs(int threshold) {
+void serial_bfs() {
+    int i, j, k;
+    push(0);
+    visit(0);
+    while (size != 0) {
+        uint128_t u = pop();
+        u = (((((((u << 25) | u) << 25) | u) << 25) | u) << 25) | u;
+        for (i = 4; i >= 0; --i) {
+            // loop unrolling x5 + packed 128-bit XOR
+            uint128_t x = u ^ cmask[i];
+            int e = x & 0x1FFFFFF;
+            x >>= 25;
+            int d = x & 0x1FFFFFF;
+            x >>= 25;
+            int c = x & 0x1FFFFFF;
+            x >>= 25;
+            int b = x & 0x1FFFFFF;
+            x >>= 25;
+            int a = x;
+            k = (i << 2) + i;
+            if (!test(a)) { visit(a); prv[a] = k  ; push(a); }
+            if (!test(b)) { visit(b); prv[b] = k+1; push(b); }
+            if (!test(c)) { visit(c); prv[c] = k+2; push(c); }
+            if (!test(d)) { visit(d); prv[d] = k+3; push(d); }
+            if (!test(e)) { visit(e); prv[e] = k+4; push(e); }
+        }
+    }
+}
+
+void parallel_bfs() {
     cpu_set_t cpus;
     pthread_t threads[MAX_CORES];
     pthread_attr_t attr;
-    pthread_attr_init(&attr);
+    pthread_attr_init(&attr);    
+    int s = 25 / cores + (25 % cores != 0);
+    for (int i = 0, k = 0; i < 25; i++) {
+        curr[k][curr_size[k]++] = mask[i];
+        prv[mask[i]] = i;
+        visit(mask[i]);
+        if (curr_size[k] == s) k++;
+    }
     
-    int i, j, x, u;
-     
-    curr_size = 1;
-    curr[0] = 0;
-
     visit(0);    
-    while (curr_size != 0) {
-        // if the size of the current_level queue is feasible for parallization
-        if (curr_size >= threshold && curr_size >= cores) {
-            // start threads that consume mutually exclusive regions of the queue
-            int start = 0;
-            int size = curr_size;
-            for (i = 0; i < cores; i++) {
-                // set core affinity
-                CPU_ZERO(&cpus);
-                CPU_SET(i, &cpus);
-                pthread_attr_setaffinity_np(&attr, sizeof(cpu_set_t), &cpus);
-                // trim last size if the curr_size is not divisble by cores
-                if (i == cores - 1 && curr_size % cores != 0) size = curr_size % cores; 
-                // pack data into 64-bit pointer
-                uint64_t data = ((uint64_t) start << 34) | (((uint64_t) start + size) << 4) | i;
-                // run thread
-                pthread_create(&threads[i], &attr, partial_consumer, data);
-                start += size;
-            }
-            // set will be used to merge the next_level queues without duplicates
-            memset(set, 0, sizeof set);
-            // finish execution
-            for (i = 0; i < cores; i++) {
-                pthread_join(threads[i], NULL);
-            }
-            /*
-                If we are dealing with large queues, using memcpy will be faster
-                because it is probably optimized and avoids branch misprediction (STEP 1).
-                To avoid duplicates, we can do another loop that removes duplicates (STEP 2)
-            */
-
-            // combine the results without dealing with duplicates
-            // STEP 1: pure efficient copy
-
-            // instead of copying the first next_level queue, we swap the pointers
-            swap_pointer(&curr, &next[0]);
-            curr_size = next_size[0];
-
-            // insert first next_level queue elements into set
-            for (i = 0; i < curr_size; i++) {
-                insert(curr[i]);
-            }
-
-            // copy remaining next_level queues
-            for (i = 1; i < cores; i++) {
-                memcpy(&curr[curr_size], next[i], sizeof(int) * next_size[i]);
-                curr_size += next_size[i];
-            }
-
-            // STEP 2
-            // remove duplicates, skipping first next_level queue elements
-            for (i = next_size[0]; i < curr_size; i++) {
-                if (!check(curr[i])) {
-                    insert(curr[i]);
-                } else {
-                    curr[i] = curr[curr_size--];
-                }
-            }
-            memset(next_size, 0, sizeof next_size);
-        } else {
-            // serial processing of current level
-            for (int j = 0; j < curr_size; j++) {
-                int u = curr[j];
-                for (int i = 24; i >= 0; --i) {
-                    int x = u ^ mask[i];
-                    if (!test(x)) {
-                        next[0][next_size[0]++] = x;
-                        visit(x);
-                        prv[x] = i;
-                    }
-                }
-            }
-            curr_size = next_size[0];            
-            next_size[0] = 0;
-            swap_pointer(&curr, &next[0]);
+    while (1) {
+        // for (int i = 0; i < cores; i++) {
+        //     printf("%d ", curr_size[i]);
+        // }
+        // printf("\n");
+        for (int i = 0; i < cores; i++) {
+            CPU_ZERO(&cpus);
+            CPU_SET(i, &cpus);
+            pthread_attr_setaffinity_np(&attr, sizeof(cpu_set_t), &cpus);
+            pthread_create(&threads[i], &attr, partial_consumer, i);
         }
+        for (int i = 0; i < cores; i++) {
+            pthread_join(threads[i], NULL);
+        }
+        int sum = 0;
+         for (int i = 0; i < cores; i++) {
+            swap_pointer(&curr[i], &next[i]);
+            curr_size[i] = next_size[i];
+            sum += curr_size[i];
+            next_size[i] = 0;
+        }
+        if (sum == 0) break;
     }
 }
 
@@ -205,41 +203,62 @@ uint32_t open() {
     return board;
 }
 
-int run_tests() {
-    struct timespec start, finish;
-    double elapsed;
-    for (int threshold = 100; threshold <= 1e6; threshold*=10) {
-        for (cores = 1; cores <= 4; cores++) {
-            printf("Cores: %d, Threshold: %d\n", cores, threshold);
-            memset(prv, 25, sizeof prv);
-            memset(visited, 0, sizeof visited);
-            memset(set, 0, sizeof set);
-            memset(next_size, 0, sizeof next_size);
-            for (int i = 0; i < cores; i++) next[i] = arr2[i];
-            curr = arr1;
-            
-            uint32_t board = open();
-            clock_gettime(CLOCK_MONOTONIC, &start);            
-            bfs(threshold);
-            clock_gettime(CLOCK_MONOTONIC, &finish);
-            elapsed = (finish.tv_sec - start.tv_sec);
-            elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
-
-            // render(board);
-            // while (board != 0) {
-            //     if (prv[board] == 25) {
-            //         break;
-            //     }
-            //     board = board ^ mask[prv[board]];
-            //     render(board);        
-            // }
-            printf("%.2f\n", elapsed);
-            fprintf(stderr, "sleeping for 5 seconds\n");
-            sleep(5);
-        }
-    }
-}
-
 int main() {
+    struct timespec _start, _finish;
+    double elapsed;
+    
+    // get board
+    uint32_t board = open();
+    
+    // initialize memory needed
+    cores = 4;
+    memset(prv, 25, sizeof prv);
+    memset(visited, 0, sizeof visited);
+    memset(next_size, 0, sizeof next_size);
+    memset(curr_size, 0, sizeof curr_size);
+    
+    for (int i = 0; i < cores; i++) 
+        next[i] = arr2[i],
+        curr[i] = arr1[i];
+    
+    // benchmark parallel
+    clock_gettime(CLOCK_MONOTONIC, &_start);            
+    parallel_bfs();
+    clock_gettime(CLOCK_MONOTONIC, &_finish);
+    elapsed = (_finish.tv_sec - _start.tv_sec);
+    elapsed += (_finish.tv_nsec - _start.tv_nsec) / 1000000000.0;
+    printf("Parallel Runtime: %.2f\n", elapsed);
+    fprintf(stderr, "sleeping for 5 seconds\n");
 
+    // render(board);
+    // while (board != 0) {
+    //     if (prv[board] == 25) {
+    //         break;
+    //     }
+    //     board = board ^ mask[prv[board]];
+    //     render(board);        
+    // }
+    sleep(5);
+    
+    // initialize memory needed
+    memset(visited, 0, sizeof visited);
+    memset(prv, 25, sizeof prv);
+    start = size = 0;
+    // benchmark
+    clock_gettime(CLOCK_MONOTONIC, &_start);            
+    serial_bfs();
+    clock_gettime(CLOCK_MONOTONIC, &_finish);
+    elapsed = (_finish.tv_sec - _start.tv_sec);
+    elapsed += (_finish.tv_nsec - _start.tv_nsec) / 1000000000.0;
+    printf("Serial Runtime: %.2f\n", elapsed);
+    
+
+    // render(board);
+    // while (board != 0) {
+    //     if (prv[board] == 25) {
+    //         break;
+    //     }
+    //     board = board ^ mask[prv[board]];
+    //     render(board);        
+    // }
 }
